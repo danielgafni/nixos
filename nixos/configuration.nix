ac1a5dbd121ce3964964cdce8b912fb24359f739
Daniel Gafni <danielgafni16@gmail.com>
2023-09-13 18:40:15 +0200

    use fira code nerd font

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -55,7 +55,7 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
-    font = "FiraCode";
+    font = "FiraCode Nerd Font";
     # keyMap = "us";
     useXkbConfig = true; # use xkbOptions in tty.
   };
