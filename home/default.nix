85deb6aa8ea85c28391c82d916e68c3aca901d04
Daniel Gafni <danielgafni16@gmail.com>
2023-10-26 13:36:47 +0200

    configure git

--- home/default.nix
+++ home/default.nix
@@ -151,6 +151,10 @@
     enable = true;
     userName = "danielgafni";
     userEmail = "danielgafni16@gmail.com";
+    extraConfig = {
+        commit.gpgsign = true;
+        user.signingkey = "7B0740201D518DB134D5C75AB8D13360DED17662";
+      };
   };
 
   # systemd.user,targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target"  ];
