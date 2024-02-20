9d541139b4895742043703043ff818b0ec78a8f9
Daniel Gafni <danielgafni16@gmail.com>
2024-02-21 00:16:49 +0100

    :star: add bat

--- home/default.nix
+++ home/default.nix
@@ -46,6 +46,25 @@
 
   colorScheme = inputs.nix-colors.colorSchemes.catppuccin-mocha;
   programs.dircolors.enable = true;
+  programs.bat = {
+    enable = true;
+    config = {
+      pager = "less -FR";
+      theme = "CatppuccinMocha";
+    };
+    extraPackages = with pkgs.bat-extras; [ batdiff batman batgrep batwatch ];
+    themes = {
+          CatppuccinMocha = {
+            src = pkgs.fetchFromGitHub {
+              owner = "catppuccin";
+              repo = "bat"; # Bat uses sublime syntax for its themes
+              rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+              sha256 = "1g2r6j33f4zys853i1c5gnwcdbwb6xv5w6pazfdslxf69904lrg9";
+            };
+            file = "Catppuccin-mocha.tmTheme";
+          };
+        };
+  };
 
   fonts.fontconfig.enable = true;
 
@@ -99,11 +118,11 @@
     zsh
     starship
     zellij
-    bat
     eza
     pfetch
     neofetch
     pre-commit
+    strace
 
     # fonts
     (
