300713936f75f7e05bb3cbac6283ef811a488814
danielgafni <danielgafni16@gmail.com>
2024-03-19 22:47:37 +0100

    :book: update docs

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -239,6 +239,14 @@
 
   # YubiKey
   services.udev.packages = [pkgs.yubikey-personalization];
+  services.yubikey-agent.enable = false;
+  programs.ssh.startAgent = false;  # using gpg agent instead
+
+  environment.shellInit = ''
+    gpg-connect-agent /bye
+    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+  '';
+
   services.pcscd.enable = true;
 
   # Screen Sharing
