891caddc97f9866db8bf1373b3f50f2c0da3af51
Daniel Gafni <danielgafni16@gmail.com>
2024-05-13 21:59:48 +0200

    :star: configure neovim

--- home/default.nix
+++ home/default.nix
@@ -18,22 +18,13 @@
     ./eww.nix
     ./hyprpaper.nix
     ./vscode.nix
+    ./neovim.nix
   ];
 
   nixpkgs = {
-    # You can add overlays here
     overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
       inputs.nixpkgs-wayland.overlay
     ];
-    # Configure your nixpkgs instance
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
@@ -69,9 +60,6 @@
       };
     };
   };
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
 
   home.packages = with pkgs; [
     # YubiKey
@@ -161,7 +149,6 @@
 
     # editors & IDE
     vim
-    neovim
     helix
     jetbrains.pycharm-professional
 
---
+++ home/neovim.nix
@@ -0,0 +1,38 @@
+{pkgs, ...}: {
+  programs.nixvim = {
+    enable = true;
+    clipboard.providers.wl-copy.enable = true;
+    colorschemes.catppuccin.enable = true;
+    colorschemes.catppuccin.settings = {
+      flavour = "mocha";
+      integrations = {
+        cmp = true;
+        gitsigns = true;
+        mini = true;
+        notify = true;
+        nvimtree = true;
+        treesitter = true;
+      };
+    };
+
+    plugins.nix.enable = true;
+    plugins.lightline.enable = true;
+    plugins.cmp.enable = true;
+    plugins.cmp-path.enable = true;
+    plugins.cmp-buffer.enable = true;
+    plugins.cmp-cmdline.enable = true;
+    plugins.copilot-cmp.enable = true;
+
+    plugins.treesitter.enable = true;
+
+    plugins.lsp.enable = true;
+    plugins.lsp-format.enable = true;
+
+    opts = {
+      number = true; # Show line numbers
+      relativenumber = true; # Show relative line numbers
+
+      shiftwidth = 4;
+    };
+  };
+}
