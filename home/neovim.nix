6a76390e1c9429e67caee5ace911cf5fd1c69981
Daniel Gafni <danielgafni16@gmail.com>
2024-05-14 12:02:33 +0200

    add lsp

--- home/neovim.nix
+++ home/neovim.nix
@@ -31,7 +31,20 @@
     plugins.chadtree.enable = true;
     plugins.bufferline.enable = true;
 
-    plugins.lsp.enable = true;
+    plugins.lsp = {
+      enable = true;
+      servers = {
+        nixd.enable = true;
+        ruff.enable = true;
+        pyright.enable = true;
+        yamlls.enable = true;
+        jsonls.enable = true;
+        helm-ls.enable = true;
+        dockerls.enable = true;
+        docker-compose-language-service.enable = true;
+        terraformls.enable = true;
+      };
+    };
     plugins.lsp-format.enable = true;
     plugins.helm.enable = true;
 
@@ -41,5 +54,12 @@
 
       shiftwidth = 4;
     };
+
+    keymaps = [
+      {
+        key = "<leader>t";
+        action = "<cmd>:CHADopen<CR>";
+      }
+    ];
   };
 }
