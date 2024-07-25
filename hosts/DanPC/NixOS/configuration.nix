062e13379f6c8f5615ef285ae275a724c2340d87
danielgafni <danielgafni16@gmail.com>
2024-07-25 16:20:40 +0200

    try different nvidia driver

--- hosts/DanPC/NixOS/configuration.nix
+++ hosts/DanPC/NixOS/configuration.nix
@@ -1,7 +1,11 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   networking.hostName = "DanPC";
 
   services.xserver.videoDrivers = ["nvidia"];
@@ -14,6 +18,24 @@
       open = false;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.production;
+      hardware.nvidia.package = let
+        # pin 535 driver version until 550's issues are resolved
+        # https://nixos.wiki/wiki/Nvidia#Running_the_new_RTX_SUPER_on_nixos_stable
+        rcu_patch = pkgs.fetchpatch {
+          url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+          hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+        };
+      in
+        config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "535.154.05";
+          sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+          sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+          openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+          settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+          persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
+
+          patches = [rcu_patch];
+        };
     };
     opengl = {
       enable = true;
