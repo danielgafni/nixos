0f5851a3bf61a50cc5331f8433fb7e60c077687f
danielgafni <danielgafni16@gmail.com>
2024-10-15 16:50:52 +0200

    add ssh hosts

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -6,6 +6,7 @@ _: {
       devbox = {
         hostname = "16.171.214.85";
         user = "dan";
+        #        RemoteForward = "/run/user/1000/gnupg/S.gpg-agent /run/user/1001/gnupg/S.gpg-agent.extra";
       };
       skynet = {
         hostname = "76.126.173.16";
