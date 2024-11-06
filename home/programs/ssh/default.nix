8145e4a41e985d6995bbfb56d66a3fe4f746a1e2
Daniel Gafni <danielgafni16@gmail.com>
2024-11-06 03:15:22 +0200

    :bug: fix gpg agent forwarding with yubikey (#107)

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -2,11 +2,14 @@ _: {
   programs.ssh = {
     enable = true;
     forwardAgent = true;
+    extraConfig = ''
+      RemoteForward /run/user/1001/gnupg/S.gpg-agent /run/user/1001/gnupg/S.gpg-agent.extra
+      RemoteForward /run/user/1001/gnupg/S.gpg-agent.ssh /run/user/1001/gnupg/S.gpg-agent.ssh
+    '';
     matchBlocks = {
       devbox = {
         hostname = "16.171.214.85";
         user = "dan";
-        #        RemoteForward = "/run/user/1000/gnupg/S.gpg-agent /run/user/1001/gnupg/S.gpg-agent.extra";
       };
       skynet = {
         hostname = "76.126.173.16";
--- home/services/gpg-agent/default.nix
+++ home/services/gpg-agent/default.nix
@@ -4,6 +4,9 @@
     enableSshSupport = true;
     enableExtraSocket = true;
     pinentryPackage = pkgs.pinentry-curses;
+    extraConfig = ''
+      extra-socket /run/user/1001/gnupg/S.gpg-agent.extra
+    '';
     #extraConfig = ''
     #  pinentry-mode loopback
     #  allow-loopback-pinentry
