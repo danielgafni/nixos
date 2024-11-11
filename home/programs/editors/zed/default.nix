fb3b6acb0aca5c7375c5c9d9f5fdc66d8670fc8d
danielgafni <danielgafni16@gmail.com>
2024-11-11 18:09:22 +0100

    :recycle: nifixy zed settings

--- home/programs/editors/zed/default.nix
+++ home/programs/editors/zed/default.nix
@@ -11,5 +11,52 @@
       "docker-compose"
       "git-firefly"
     ];
+    userSettings = {
+      auto_update = false;
+      buffer_font_family = "FiraCode Nerd Font Mono";
+      buffer_font_features = {
+        calt = true;
+      };
+      ui_font_size = 16;
+      buffer_font_size = 16;
+      theme = {
+        mode = "system";
+        light = "Catppuccin Espresso (Blur)";
+        dark = "Catppuccin Espresso (Blur)";
+      };
+      terminal = {
+        font_family = "ZedMono Nerd Font";
+      };
+      ssh_connections = [
+        {
+          host = "devbox";
+          projects = [
+            {
+              paths = ["~/dagster"];
+            }
+            {
+              paths = ["~/dagster-ray"];
+            }
+            {
+              paths = ["~/dagster-io/internal"];
+            }
+          ];
+        }
+      ];
+      languages = {
+        Python = {
+          language_servers = ["ruff" "pyright"];
+        };
+      };
+      lsp = {
+        pyright = {
+          settings = {
+            python = {
+              pythonPath = ".venv/bin/python";
+            };
+          };
+        };
+      };
+    };
   };
 }
