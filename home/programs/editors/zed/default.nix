8df9423aca5dd8b7dccf5871f7ab43466022d4bc
danielgafni <danielgafni16@gmail.com>
2024-11-11 12:08:01 +0100

    :wrench: configure zed

--- home/programs/editors/zed/default.nix
+++ home/programs/editors/zed/default.nix
@@ -1,9 +1,15 @@
 {inputs, ...}: {
-  home.packages = [
-    inputs.zed.packages.x86_64-linux.zed-editor
-  ];
-
-  xdg.configFile."zed/settings.json" = {
-    source = ./settings.json;
+  programs.zed-editor = {
+    enable = true;
+    package = inputs.zed.packages.x86_64-linux.zed-editor;
+    # read settings.json
+    userSettings = builtins.fromJSON (builtins.readFile ./settings.json);
+    extensions = [
+      "catppuccin-blur"
+      "nix"
+      "dockerfile"
+      "docker-compose"
+      "git-firefly"
+    ];
   };
 }
