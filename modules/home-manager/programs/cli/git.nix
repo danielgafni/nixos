67fd08f374f803c8e7e96143ea6a01830735e0d4
danielgafni <danielgafni16@gmail.com>
2025-11-24 10:43:38 +0200

    :arrow_up: update flake

--- modules/home-manager/programs/cli/git.nix
+++ modules/home-manager/programs/cli/git.nix
@@ -1,39 +1,39 @@
 {userConfig, ...}: {
-  programs.gitui = {
-    enable = true;
-  };
-
-  programs.delta = {
-    enable = true;
-    enableGitIntegration = true;
-    options = {
-      side-by-side = true;
-      whitespace-error-style = "22 reverse";
-    };
-  };
-
-  programs.git =
-    if builtins.hasAttr "git" userConfig
-    then {
+  programs = {
+    gitui = {
       enable = true;
-
-      signing.key = userConfig.git.signingkey;
-
-      settings = {
-        inherit (userConfig.git) user;
-        commit.gpgsign = true;
-        # the below options are mostly taken from https://jvns.ca/blog/2024/02/16/popular-git-config-options/
-        push.autosetupremote = true;
-        push.default = "current";
-        pull.rebase = true;
-        fetch.prune = true;
-        init.defaultBranch = "main";
-        merge.conflictstyle = "diff3";
-        rerere.enabled = true;
-        diff.algorithm = "histogram";
-      };
-    }
-    else {
-      enable = false;
     };
+    delta = {
+      enable = true;
+      enableGitIntegration = true;
+      options = {
+        side-by-side = true;
+        whitespace-error-style = "22 reverse";
+      };
+    };
+    git =
+      if builtins.hasAttr "git" userConfig
+      then {
+        enable = true;
+
+        signing.key = userConfig.git.signingkey;
+
+        settings = {
+          inherit (userConfig.git) user;
+          commit.gpgsign = true;
+          # the below options are mostly taken from https://jvns.ca/blog/2024/02/16/popular-git-config-options/
+          push.autosetupremote = true;
+          push.default = "current";
+          pull.rebase = true;
+          fetch.prune = true;
+          init.defaultBranch = "main";
+          merge.conflictstyle = "diff3";
+          rerere.enabled = true;
+          diff.algorithm = "histogram";
+        };
+      }
+      else {
+        enable = false;
+      };
+  };
 }
