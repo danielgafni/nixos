8beae4074363ed3e5aa2952d36c9e984d53fca43
Daniel Gafni <danielgafni16@gmail.com>
2026-02-05 18:36:08 +0200

    :lipstick: show github PR link in starship prompt (#285)

--- modules/home-manager/shared/shell/starship.nix
+++ modules/home-manager/shared/shell/starship.nix
@@ -34,6 +34,19 @@
         disabled = true;
         symbol = "🇬️";
       };
+
+      custom.github_pr = {
+        command = ''
+          cache_dir="''${XDG_CACHE_HOME:-$HOME/.cache}/starship"
+          repo_id=$(git rev-parse --show-toplevel 2>/dev/null | md5sum | cut -c1-8)
+          cache_file="$cache_dir/gh_pr_$repo_id"
+          [ -f "$cache_file" ] && cat "$cache_file"
+        '';
+        when = "git rev-parse --git-dir 2>/dev/null";
+        format = "[$output]($style) ";
+        style = "cyan";
+        shell = ["bash" "--noprofile" "--norc"];
+      };
     };
   };
 }
