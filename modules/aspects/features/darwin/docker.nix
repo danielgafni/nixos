07ef4d17c2e017ac8b0eee55b01ca88cee6440aa
Daniel Gafni <danielgafni16@gmail.com>
2026-07-17 09:40:05 +0300

    feat(darwin): use vz instead of qemu for colima (#317)

--- modules/aspects/features/darwin/docker.nix
+++ modules/aspects/features/darwin/docker.nix
@@ -15,7 +15,11 @@ _: {
       environment.variables.DOCKER_HOST = "unix:///Users/${config.system.primaryUser}/.colima/default/docker.sock";
 
       launchd.user.agents.colima = {
-        command = "${pkgs.colima}/bin/colima start --foreground --memory 16 --root-disk 500";
+        # vz (Virtualization.framework) instead of the default qemu backend:
+        # nixpkgs qemu 11.0.0 crashes at startup on macOS 26 (HVF assertion
+        # on HV_SYS_REG_SMCR_EL1), and vz is faster anyway. Switching an
+        # existing qemu-created VM requires `colima delete` first.
+        command = "${pkgs.colima}/bin/colima start --foreground --vm-type vz --memory 16 --root-disk 500";
         path = [pkgs.docker pkgs.colima "/usr/bin" "/bin" "/usr/sbin"];
         serviceConfig = {
           Label = "com.github.abiosoft.colima";
